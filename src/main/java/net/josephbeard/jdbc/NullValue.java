dd749ae1649162b86607db431419b17eab2d15f4
Joseph Beard <joseph@josephbeard.net>
2018-03-29 20:14:44 -0400

    Missing toString(), hashCode(), and equals() methods.

--- src/main/java/net/josephbeard/jdbc/NullValue.java
+++ src/main/java/net/josephbeard/jdbc/NullValue.java
@@ -18,4 +18,31 @@ class NullValue implements ParameterValue {
     public void applyValue(PreparedStatement statement, int parameterIndex) throws SQLException {
         statement.setNull(parameterIndex, sqlType);
     }
+
+    @Override
+    public String toString() {
+        return "(" + sqlType + ") NULL";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + sqlType;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NullValue other = (NullValue) obj;
+        if (sqlType != other.sqlType)
+            return false;
+        return true;
+    }
 }
