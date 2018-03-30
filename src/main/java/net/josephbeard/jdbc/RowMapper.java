30795a58f9a1baa1a907cb11aa33833fc21e3d66
Joseph Beard <joseph@josephbeard.net>
2018-03-29 20:15:11 -0400

    Convenience method to consume each row of a ResultSet.

--- src/main/java/net/josephbeard/jdbc/RowMapper.java
+++ src/main/java/net/josephbeard/jdbc/RowMapper.java
@@ -2,6 +2,9 @@ package net.josephbeard.jdbc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.function.Consumer;
+
+import org.apache.commons.lang3.Validate;
 
 /**
  * Maps a single row from a {@link ResultSet}.
@@ -21,5 +24,31 @@ public interface RowMapper<T> {
      *             if an error occurs
      */
     T processRow(ResultSet resultSet, long rowNumber) throws SQLException;
+
+    /**
+     * Create a {@link RowConsumer} that first maps according to this
+     * {@link RowMapper} and supplies the mapped value to the {@link Consumer}. For
+     * example:
+     * 
+     * <pre>
+     * RowMapper&lt;String&gt; rowMapper = (resultSet, rowNumber) -&gt; resultSet.getString("name");
+     *
+     * jdbcTemplate.forEach("SELECT name FROM people;",
+     *      rowMapper.mappedRowConsumer(name -&gt; System.out.println("Hello, " + name));
+     * </pre>
+     *
+     * @param consumer
+     *            the mapped row consumer
+     * @return the row consumer
+     * @see JdbcTemplate#forEach(String, RowConsumer, ParameterValue...)
+     */
+    default RowConsumer consume(Consumer<? super T> consumer) {
+        Validate.notNull(consumer, "The consumer must not be null");
+
+        return (resultSet, rowNumber) -> {
+            T row = processRow(resultSet, rowNumber);
+            consumer.accept(row);
+        };
+    }
 
 }
